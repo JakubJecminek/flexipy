08c474c9e1e068d8c8328faabde2e1d837796f02
Jakub Ječmínek <kuba@devLubuntu.(none)>
2013-03-23 13:46:44 +0100

    opraven test

--- test_flexipy/test_main.py
+++ test_flexipy/test_main.py
@@ -1,16 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from flexipy import main
-
+import pytest
 
 def test_validate_item():	
-
-	valid_params = {'doprava':'','duzpPuv':'','zavTxt':''}
-	#valid_params by meli projit validaci a vrati (True,None)
-	expected_valid = (True, None)
-	assert main.__validate_params(valid_params, 'faktura-vydana') == expected_valid
-	invalid_params = {'doprava':'','duzpaPuv':'','zaveTxt':''}
-	expected_invalid = (False, ['duzpaPuv','zaveTxt'])
-	assert main.__validate_params(invalid_params, 'faktura-vydana') == expected_invalid
+	invalid_params = {'doprava':'','duzpaPuv':'','zaveTxt':''}	
+	pytest.raises(main.FlexipyException,main.__validate_params, invalid_params, 'faktura-vydana') 
 
 
